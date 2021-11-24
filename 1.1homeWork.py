5099ff01e5bc3113e783c9e2c3062508667d3cc1
mafiozy39 <57320920+mafiozy39@users.noreply.github.com>
2021-11-24 09:48:12 +0300

    Update 1.1homeWork.py

--- 1.1homeWork.py
+++ 1.1homeWork.py
@@ -1,9 +1,13 @@
 def func1():
-    spisok=[]
-    n=1
+    spisok = []
+    n = 1
     while n:
         n = input("Введите что-нибудь: ")
+        if n == '' or n == ' ':
+            break
         spisok.append(n)
 
     return spisok
+
+
 print(func1())
