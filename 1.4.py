98868b19942549b20e4e6a607b3b105fc0c7fb2c
mafiozy39 <57320920+mafiozy39@users.noreply.github.com>
2021-11-24 10:26:00 +0300

    Add files via upload

---
+++ 1.4.py
@@ -0,0 +1,21 @@
+
+
+def fibb(n):
+    list=[]
+    i=int(n)
+    temp=0
+    last=0
+    new=1
+    while i!=0:
+        i -= 1
+        temp=new
+        list.append(last)
+        new=last+new
+        last=temp
+
+    print("first",n,"number(s) of Fibonacci numbers: ")
+    return list
+
+
+a=input("Put the number of members Fibbonaci's numbers: ")
+print(fibb(a))
