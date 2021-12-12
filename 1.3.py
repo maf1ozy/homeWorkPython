2d60c73530c910e33c8cc298094b7dc1e3aba32c
mafiozy39 <57320920+mafiozy39@users.noreply.github.com>
2021-12-12 13:40:16 +0300

    Update 1.3.py

--- 1.3.py
+++ 1.3.py
@@ -1,21 +1,19 @@
-
-
 def fibb(n):
-    list=[]
-    i=int(n)
-    temp=0
-    last=0
-    new=1
-    while i!=0:
+    list = []
+    i = int(n)
+    temp = 0
+    last = 0
+    new = 1
+    while i != 0:
         i -= 1
-        temp=new
+        temp = new
         list.append(last)
-        new=last+new
-        last=temp
+        new = last+new
+        last = temp
 
-    print("first",n,"number(s) of Fibonacci numbers: ")
+    print("first", n, "number(s) of Fibonacci's numbers: ")
     return list
 
 
-a=input("Put the number of members Fibbonaci's numbers: ")
+a = input("Put the number of members Fibonacci's numbers: ")
 print(fibb(a))
