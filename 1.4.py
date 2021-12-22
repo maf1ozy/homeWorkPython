3e301ec05116c94f64ad4d3f9d5d9bb7eb393a97
mafiozy39 <57320920+mafiozy39@users.noreply.github.com>
2021-12-22 18:23:50 +0300

    Update 1.4.py

--- 1.4.py
+++ 1.4.py
@@ -1,20 +1,20 @@
 import first
 
 
-list=func1()
+list = func1()
 print("element  |  chastota")
-count=0
-counted=[]
+count = 0
+counted = []
 for elem in list:
-    marker=1;
+    marker = 1;
     for rep in counted:
         if elem == rep:
-            marker=0
-    if marker==0:
+            marker = 0
+    if marker == 0:
         continue
     for elem1 in list:
-        if elem==elem1:
-            count+=1
+        if elem == elem1:
+            count += 1
             counted.append(elem)
-    print ("   ",elem,"   |  ", count)
-    count=0
+    print ("   ", elem, "   |  ", count)
+    count = 0
