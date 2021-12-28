1243b1de874d43e7d0b08528423e8d050def46cd
mafiozy39 <57320920+mafiozy39@users.noreply.github.com>
2021-12-28 05:35:35 +0300

    Update 1.5.py

--- 1.5.py
+++ 1.5.py
@@ -1,22 +1,10 @@
-def vvod():
-    list=[]
-    n=1
-    while n:
-        n=input("put your number: ")
-        if n:
-            list.append(int(n))
-    return list
+from first import func1
 
-def avrg(lis):
-    spisok=lis
-    counter=0
-    sum=0
-    for ele in spisok:
-        sum+=ele
-        counter+=1
-    res = sum/counter
-    return res
+def avrg(nums):
+    return sum(nums)/len(nums)
 
-list=vvod()
-res = avrg(list)
-print("Srednee arifmeticheskoe = ",res)
+
+list = func1()
+numbers = [int(elem) for elem in list]
+res = avrg(numbers)
+print("Srednee arifmeticheskoe = ", res)
