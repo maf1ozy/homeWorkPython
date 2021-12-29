8260643f883193b956ec6f3f5ad0404f2ec01c91
mafiozy39 <57320920+mafiozy39@users.noreply.github.com>
2021-12-29 08:18:40 +0300

    Update dz16.py

--- dz16.py
+++ dz16.py
@@ -1,10 +1,12 @@
 def whatNumber(n):
-    marker=True
+    marker = True
+    if n == 1 or n == 0:
+        return False
     for num in range(n):
-        if num!=0 and num!=1 and num!=n and n%num==0:
+        if num != 0 and num != 1 and num != n and n%num == 0:
             marker = False
 
     return marker
 
-a=input("put your number: ")
+a = input("put your number: ")
 print(whatNumber(int(a)))
